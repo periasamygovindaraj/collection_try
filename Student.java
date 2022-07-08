29582008ccda4a55401d3086a0f4a59b379567e8
periasamygovindaraj <102314168+periasamygovindaraj@users.noreply.github.com>
2022-07-08 17:24:42 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+package com.framework.collection;
+
+public class Student {
+	String name;
+	int tamil;
+	int english;
+	int maths;
+	int science;
+	int social;
+	
+	public Student(String name,int tamil,int english,int maths,int science,int social)
+	{
+		this.name=name;
+		this.tamil=tamil;
+		this.english=english;
+		this.maths=maths;
+		this.science=science;
+		this.social=social;
+	}
+}
